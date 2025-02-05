c8e63973b6bb789ec91357c49d4dfdeabffd2dae
ABDELHAFID AIT EL MOKHTAR <103076950+ABDELHAFIDAIT@users.noreply.github.com>
2025-02-05 17:04:51 +0100

    Add config.php & Database Class (Connexion to DB)

---
+++ config/config.php
@@ -0,0 +1,7 @@
+<?php
+
+define("DB_HOST" , 'localhost');
+define("DB_NAME" , 'cabinet_medical');
+define("DB_USER" , 'postgres');
+define("DB_PASS" , '902320');
+define("DB_CHARSET" , 'utf8mb4');
---
+++ core/Database.php
@@ -0,0 +1,55 @@
+<?php
+
+require_once __DIR__ . './../config/config.php';
+
+class Database {
+    private static ?Database $instance = null;
+    private $host;
+    private $db;
+    private $user;
+    private $pass;
+    private $charset;
+    private $pdo;
+
+    private function __construct() {
+        $this->host = DB_HOST;
+        $this->db = DB_NAME;
+        $this->user = DB_USER;
+        $this->pass = DB_PASS;
+        $this->charset = DB_CHARSET;
+
+        $this->connect();
+    }
+
+    
+    private function connect() {
+        $dsn = "pgsql:host=$this->host;dbname=$this->db;charset=$this->charset";
+        $options = [
+            PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+            PDO::ATTR_EMULATE_PREPARES   => false,
+        ];
+
+        try {
+            $this->pdo = new PDO($dsn, $this->user, $this->pass, $options);
+        } catch (PDOException $e) {
+            error_log($e->getMessage(), 3, '../logs/errors.log');
+            throw new PDOException("Error connecting to the database.");
+        }
+    }
+
+    
+    public static function getInstance(): Database {
+        if (self::$instance === null) {
+            self::$instance = new self();
+        }
+
+        return self::$instance;
+    }
+
+    
+    public function getConnection() {
+        return $this->pdo;
+    }
+}
+
